1c747c168afb82f65523d03282eb62f5c38b31c2
chetanJain19 <146300962+chetanJain19@users.noreply.github.com>
2023-11-01 19:14:28 +0530

    Update tomcatScript.sh

--- files/tomcatScript.sh
+++ files/tomcatScript.sh
@@ -5,13 +5,16 @@ sudo apt install default-jdk -y
 sudo apt-cache search tomcat
 sudo apt install tomcat9 tomcat9-admin -y
 sudo ufw allow from any to any port 9090 proto tcp
-
+rm -rf /etc/tomcat9/server.xml
+rm -rf /etc/tomcat9/tomcat-users.xml
+sudo cp /home/ubuntu/tomct/server.xml /etc/tomcat9/
+sudo cp /home/ubuntu/tomct/tomcat-users.xml /etc/tomcat9/
 #change the port number from 8080 to 9090
 
 #sudo sed -i 's/Connector port="8080"/Connector port="9090"/Ig' /etc/tomcat9/server.xml
 
 #inside tomcat-users.xml paste below commands and allign properly
 
-sudo sed -i '56 i <role rolename="admin-gui"/>' /etc/tomcat9/tomcat-users.xml
-sudo sed -i '57 i <role rolename="manager-gui"/>' /etc/tomcat9/tomcat-users.xml
-sudo sed -i '58 i <user username="tomcat" password="pass" roles="admin-gui,manager-gui"/>' /etc/tomcat9/tomcat-users.xml
+#sudo sed -i '56 i <role rolename="admin-gui"/>' /etc/tomcat9/tomcat-users.xml
+#sudo sed -i '57 i <role rolename="manager-gui"/>' /etc/tomcat9/tomcat-users.xml
+#sudo sed -i '58 i <user username="tomcat" password="pass" roles="admin-gui,manager-gui"/>' /etc/tomcat9/tomcat-users.xml
