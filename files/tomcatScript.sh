0ed96194de2d7db302bdc036be9a1f8612abde4d
chetanJain19 <146300962+chetanJain19@users.noreply.github.com>
2023-11-01 19:17:06 +0530

    Update tomcatScript.sh

--- files/tomcatScript.sh
+++ files/tomcatScript.sh
@@ -5,8 +5,8 @@ sudo apt install default-jdk -y
 sudo apt-cache search tomcat
 sudo apt install tomcat9 tomcat9-admin -y
 sudo ufw allow from any to any port 9090 proto tcp
-rm -rf /etc/tomcat9/server.xml
-rm -rf /etc/tomcat9/tomcat-users.xml
+sudo rm -rf /etc/tomcat9/server.xml
+sudo rm -rf /etc/tomcat9/tomcat-users.xml
 sudo cp /home/ubuntu/tomct/server.xml /etc/tomcat9/
 sudo cp /home/ubuntu/tomct/tomcat-users.xml /etc/tomcat9/
 #change the port number from 8080 to 9090
