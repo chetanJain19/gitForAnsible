b06fc36b57a73dbda1d0d44275e55ffb6212754b
chetanJain19 <146300962+chetanJain19@users.noreply.github.com>
2023-11-01 21:14:41 +0530

    Update tomcatScript.sh

--- files/tomcatScript.sh
+++ files/tomcatScript.sh
@@ -5,11 +5,13 @@ sudo apt install default-jdk -y
 sudo apt-cache search tomcat
 sudo apt install tomcat9 tomcat9-admin -y
 sudo ufw allow from any to any port 9090 proto tcp
-sudo cp /etc/tomcat9/server.xml /etc/tomcat9/server.xml.bak  # Create a backup of the original file
-sudo cp /tmp/tomcat-install/templates/serverxml.j2 /etc/tomcat9/server.xml
 
-if [ -f /etc/tomcat9/tomcat-users.xml ]; then
-    sudo sed -i 'some_edit_command' /etc/tomcat9/tomcat-users.xml
-else
-    echo "tomcat-users.xml does not exist. Skipping the edit."
-fi
+#change the port number from 8080 to 9090
+
+#sudo sed -i 's/Connector port="8080"/Connector port="9090"/Ig' /etc/tomcat9/server.xml
+
+#inside tomcat-users.xml paste below commands and allign properly
+
+sudo sed -i '56 i <role rolename="admin-gui"/>' /etc/tomcat9/tomcat-users.xml
+sudo sed -i '57 i <role rolename="manager-gui"/>' /etc/tomcat9/tomcat-users.xml
+sudo sed -i '58 i <user username="tomcat" password="pass" roles="admin-gui,manager-gui"/>' /etc/tomcat9/tomcat-users.xml
